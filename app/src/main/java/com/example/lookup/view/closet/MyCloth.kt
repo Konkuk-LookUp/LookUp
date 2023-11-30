56bbb30c5432c266d89120ffd3eaee553255c9cd
songhyun9999 <songhyun0323@konkuk.ac.kr>
2023-11-30 14:34:47 +0900

    dataclass MyCloth 에 filename 추가했습니다

--- app/src/main/java/com/example/lookup/view/closet/MyCloth.kt
+++ app/src/main/java/com/example/lookup/view/closet/MyCloth.kt
@@ -2,4 +2,4 @@ package com.example.lookup.view.closet
 
 import java.io.Serializable
 
-data class MyCloth(var img: Int, var name:String): Serializable
+data class MyCloth(var img: Int, var name:String,var filename:String): Serializable
